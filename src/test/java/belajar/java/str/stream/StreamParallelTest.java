7cf629f40caaa86910826329edd74ec147b7c51f
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-05-27 06:25:50 +0700

    update: Stream(Parallel)

---
+++ src/test/java/belajar/java/str/stream/StreamParallelTest.java
@@ -0,0 +1,35 @@
+package belajar.java.str.stream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamParallelTest {
+    @Test
+    void testSequential() {
+        Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .forEach(number -> {
+                    System.out.println(Thread.currentThread().getName() + " : " + number);
+                });
+    }
+
+    @Test
+    void testParallel() {
+        Stream.of(1,2,3,4,5,6,7,8,9,10)
+                .parallel()
+                .forEach(number -> {
+                    System.out.println(Thread.currentThread().getName() + " : " + number);
+                });
+    }
+
+    @Test
+    void testParallelStream() {
+        List<Integer> numbers = List.of(1,2,3,4,5,6,7,8,9,10);
+        Stream<Integer> parallelStream = numbers.stream().parallel();
+
+        parallelStream.forEach(number -> {
+            System.out.println("Thread " + Thread.currentThread() + " : " + number);
+        });
+    }
+}
